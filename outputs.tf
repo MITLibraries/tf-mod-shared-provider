4e33afc0bcfc607484ec0d489016ee94ecbb50d9
eldamit <61520319+eldamit@users.noreply.github.com>
2020-05-05 15:53:54 -0400

    mitnet alb shared outputs

--- outputs.tf
+++ outputs.tf
@@ -127,6 +127,110 @@ output "alb_public_all_ingress_sgid" {
   value       = data.terraform_remote_state.network.outputs.alb_public_all_ingress_sgid
 }
 
+##############################
+##### MITnet ALB OUTPUTS #####
+##############################
+
+# MITnet Restricted
+output "mitnet_alb_restricted_arn" {
+  description = "Restricted ALB arn"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_restricted.alb_arn
+}
+
+output "mitnet_alb_restricted_arn_suffix" {
+  description = "The ARN suffix of the ALB"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_restricted.alb_arn_suffix
+}
+
+output "mitnet_alb_restricted_name" {
+  description = "Restricted ALB name"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_restricted.alb_name
+}
+
+output "mitnet_alb_restricted_dnsname" {
+  description = "Restricted ALB DNS name"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_restricted.alb_dns_name
+}
+
+output "mitnet_alb_restricted_sgid" {
+  description = "Restricted ALB security group ID"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_restricted.security_group_id
+}
+
+output "mitnet_alb_restricted_default_target_group_arn" {
+  description = "Restricted ALB default target group arn"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_restricted.default_target_group_arn
+}
+
+output "mitnet_alb_restricted_http_listener_arn" {
+  description = "Restricted ALB HTTP listener ARN"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_restricted.http_listener_arn
+}
+
+output "mitnet_alb_restricted_https_listener_arn" {
+  description = "Restricted ALB HTTPS listener ARN"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_restricted.https_listener_arn
+}
+
+output "mitnet_alb_restricted_all_ingress_sgid" {
+  description = "Restricted ALB security group ID allowing all ingress traffic from ALB"
+  value       = data.terraform_remote_state.network.outputs.mitnet_all_access_from_alb.this_security_group_id
+}
+
+output "mitnet_alb_restricted_zone_id" {
+  description = "Restricted ALB zone id"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_restricted.alb_zone_id
+}
+
+# MITnet Public
+output "mitnet_alb_public_arn" {
+  description = "Public ALB arn"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_public.alb_arn
+}
+
+output "mitnet_alb_public_arn_suffix" {
+  description = "The ARN suffix of the ALB"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_public.alb_arn_suffix
+}
+
+output "mitnet_alb_public_name" {
+  description = "Public ALB name"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_public.alb_name
+}
+
+output "mitnet_alb_public_dnsname" {
+  description = "Public ALB DNS name"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_public.alb_dns_name
+}
+
+output "mitnet_alb_public_sgid" {
+  description = "Public ALB security group ID"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_public.security_group_id
+}
+
+output "mitnet_alb_public_default_target_group_arn" {
+  description = "Public ALB default target group arn"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_public.default_target_group_arn
+}
+
+output "mitnet_alb_public_http_listener_arn" {
+  description = "Public ALB HTTP listener ARN"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_public.http_listener_arn
+}
+
+output "mitnet_alb_public_https_listener_arn" {
+  description = "Public ALB HTTPS listener ARN"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_public.https_listener_arn
+}
+
+output "mitnet_alb_public_all_ingress_sgid" {
+  description = "Public ALB security group ID allowing all ingress traffic from ALB"
+  value       = data.terraform_remote_state.network.outputs.mitnet_all_access_from_alb_public.this_security_group_id
+}
+
+output "mitnet_alb_public_zone_id" {
+  description = "Public ALB zone id"
+  value       = data.terraform_remote_state.network.outputs.mitnet_alb_public.alb_zone_id
 #########################
 ##### Shared Global #####
 #########################
@@ -211,4 +315,3 @@ output "bastion_ingress_sgid" {
   description = "Security Group ID for access from Bastion host"
   value       = data.terraform_remote_state.bastion.outputs.ingress_from_bastion_sg_id
 }
-
